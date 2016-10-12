992afb054bddd965e997b335afdd33c6f37365d0
CBedzz <swizzlestack@gmail.com>
2016-10-11 22:57:24 -0600

    edited for eslint

--- week_1_introduction.1fdfcdbad9e3/hw1-4/app.js
+++ week_1_introduction.1fdfcdbad9e3/hw1-4/app.js
@@ -1,54 +1,55 @@
-var express = require('express'),
-    app = express(),
-    engines = require('consolidate'),
-    bodyParser = require('body-parser'),
-    MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+const express = require('express')
+const engines = require('consolidate')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const app = express()
 
-app.engine('html', engines.nunjucks);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.engine('html', engines.nunjucks)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Handler for internal server errors
 function errorHandler(err, req, res, next) {
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500).render('error_template', { error: err });
+  console.error(err.message)
+  console.error(err.stack)
+  res.status(500).render('error_template', { error: err })
 }
 
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
-    assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
+  assert.equal(null, err)
+  console.log('Successfully connected to MongoDB.')
 
-    app.get('/', function(req, res, next) {
-        res.render('add_movie', {});
-    });
-    
-    app.post('/add_movie', function(req, res, next) {
-        var title = req.body.title;
-        var year = req.body.year;
-        var imdb = req.body.imdb;
-        
-        if ((title == '') || (year == '') || (imdb == '')) {
-            next('Please provide an entry for all fields.');
-        } else {
-            db.collection('movies').insertOne(
-                { 'title': title, 'year': year, 'imdb': imdb },
-                function (err, r) {
-                    assert.equal(null, err);
-                    res.send("Document inserted with _id: " + r.insertedId);
-                }
-            );
+  app.get('/', function(req, res, next) {
+    res.render('add_Movie', {})
+  })
+
+  app.post('/add_movie', function(req, res, next) {
+    var title = req.body.title
+    var year = req.body.year
+    var imdb = req.body.imdb
+
+    if ((title == '') || (year == '') || (imdb == '')) {
+        next('Please provide an entry for all fields.')
+    } else {
+      db.collection('movies').insertOne(
+        { 'title': title, 'year': year, 'imdb': imdb },
+        function (err, r) {
+          assert.equal(null, err)
+          res.send('Document inserted with _id: ' + r.insertedId)
         }
-    });
-    
-    app.use(errorHandler);
-    
-    var server = app.listen(3000, function() {
-        var port = server.address().port;
-        console.log('Express server listening on port %s.', port);
-    });
-    
-});
+      )
+    }
+  })
+
+  app.use(errorHandler)
+
+  var server = app.listen(3000, function() {
+    var port = server.address().port
+    console.log('Express server listening on port %s.', port)
+  })
+
+})
