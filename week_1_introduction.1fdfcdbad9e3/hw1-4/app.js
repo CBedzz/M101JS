23e6358fdae05e409f41ebe214b7978500e23c3a
CBedzz <swizzlestack@gmail.com>
2016-10-11 23:15:09 -0600

    render list of all movies after post

--- week_1_introduction.1fdfcdbad9e3/hw1-4/app.js
+++ week_1_introduction.1fdfcdbad9e3/hw1-4/app.js
@@ -12,7 +12,7 @@ app.set('views', __dirname + '/views')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Handler for internal server errors
-function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res) {
   console.error(err.message)
   console.error(err.stack)
   res.status(500).render('error_template', { error: err })
@@ -23,8 +23,15 @@ MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
   assert.equal(null, err)
   console.log('Successfully connected to MongoDB.')
 
-  app.get('/', function(req, res, next) {
-    res.render('add_Movie', {})
+  app.get('/', function(req, res) {
+    res.render('add_movie', {})
+  })
+
+  app.get('/movies', (req, res) => {
+    let movies = db.collection('movies').find({}).toArray
+    db.collection('movies').find({}).toArray(function(err, movies) {
+      res.render('movies', {'movies' : movies})
+    })
   })
 
   app.post('/add_movie', function(req, res, next) {
@@ -33,13 +40,14 @@ MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
     var imdb = req.body.imdb
 
     if ((title == '') || (year == '') || (imdb == '')) {
-        next('Please provide an entry for all fields.')
+      next('Please provide an entry for all fields.')
     } else {
       db.collection('movies').insertOne(
         { 'title': title, 'year': year, 'imdb': imdb },
         function (err, r) {
           assert.equal(null, err)
-          res.send('Document inserted with _id: ' + r.insertedId)
+          //res.send('Document inserted with _id: ' + r.insertedId)
+          res.redirect('/movies')
         }
       )
     }
